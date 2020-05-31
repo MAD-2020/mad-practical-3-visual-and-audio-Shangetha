f3d36eca5307d86171e1485528d6e792c24bb91e
s10198715@connect.np.edu.sg <s10198715@connect.np.edu.sg>
2020-05-31 11:19:06 +0800

    completed week 3 practical

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView rabbidVideo = findViewById(R.id.rabbidVideo);
+        rabbidVideo.setVideoURI(Uri.parse(Uri.parse("android.resource://") + getPackageName() + "/" + R.raw.rabbid));
+        rabbidVideo.start();
     }
 }
